388028ec433749e57cc1740980d272b28e5ae26e
thaorell <khanhchuong7396@gmail.com>
2019-04-02 22:46:32 -0400

    adding buttons and proper display

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -10,27 +10,31 @@ class LandingPageState extends State with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        body: Container(
-            child: new Column(children: <Widget>[
-      new Container(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-            CircleAvatar(
-                radius: 150.0,
-                child: Image(
-                  image: new AssetImage("logo.png"),
+        body: Stack(fit: StackFit.expand, children: <Widget>[
+      Container(
+          //          margin: const EdgeInsets.symmetric(horizontal: 10.0),
+          decoration: BoxDecoration(color: Colors.lightGreen[100]),
+          child: new Column(children: <Widget>[
+            new Container(
+                margin: const EdgeInsets.only(top: 80.0),
+                child: new Image(
+                  image: new AssetImage("lib/pages/logo.png"),
                   fit: BoxFit.scaleDown,
-                ))
-          ]))
-    ])));
+                )),
+            new Text("Welcome to \n Share Sum Food",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+                textAlign: TextAlign.center),
+            new RaisedButton(
+                onPressed: _handleLogInButton, child: const Text("Log In"))
+          ])),
+    ]));
   }
 }
 
 Widget logInButton() {
   child:
   return new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 45.0),
+//      margin: const EdgeInsets.symmetric(horizontal: 45.0),
       child: new Container(
           child: new RaisedButton(
               onPressed: _handleLogInButton, child: const Text("Log In"))));
