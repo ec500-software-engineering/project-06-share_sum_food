bed37ed87b8016f384a08f82f0997665ad78e9f5
thaorell <khanhchuong7396@gmail.com>
2019-04-09 11:45:02 -0400

    adding functioanlities

--- lib/pages/log_in.dart
+++ lib/pages/log_in.dart
@@ -6,30 +6,73 @@ class LogIn extends StatefulWidget {
 }
 
 class LogInState extends State with TickerProviderStateMixin {
-  final TextEditingController _textController = new TextEditingController()
+  final TextEditingController _pwdController = new TextEditingController();
+  final TextEditingController _usrController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var passwordField = "";
     var userNameField = "";
-    var submitButton  = RaisedButton(
+    var submitButton = RaisedButton(
       child: Text("Log In"),
-      onPressed: () => _submitButtonHandler(userNameField,passwordField),
+      onPressed: () => _submitButtonHandler(userNameField, passwordField),
     );
-
+    return Scaffold(
+        body: Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        Container(
+          margin: new EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: <Widget>[
+              Text("Hello! Sign in to Share Sum Food",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black,
+                      fontSize: 20.0)),
+              Spacer(),
+              Text("Username",
+                  textAlign: TextAlign.left, style: TextStyle(fontSize: 16.0)),
+              Container(
+                  margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 0),
+                  child: _userNameBox()),
+              Text("Password",
+                  textAlign: TextAlign.left, style: TextStyle(fontSize: 16.0)),
+              Container(
+                  margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 10.0),
+                  child: _passwordBox()),
+              Container(child: submitButton),
+              Spacer()
+            ],
+          ),
+        )
+      ],
+    ));
   }
-  Widget _userNameBox(){
+
+  Widget _userNameBox() {
     var textBox = Container(
-      margin: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: new TextField(
-        controller: _textController,
-        decoration: 
-          new InputDecoration.collapsed(hintText: "Username"),
-      )
-    );
+        color: Theme.of(context).buttonColor,
+        height: 50.0,
+        margin: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: new TextField(
+          controller: _usrController,
+//          decoration: new InputDecoration.collapsed(hintText: "Username"),
+        ));
+    return textBox;
   }
 
-  void _submitButtonHandler(String usr, String pwd){
-
+  Widget _passwordBox() {
+    var textBox = Container(
+        margin: const EdgeInsets.symmetric(horizontal: 15.0),
+        color: Theme.of(context).buttonColor,
+        child: new TextField(
+          controller: _pwdController,
+//          decoration: new InputDecoration.collapsed(hintText: "Password"),
+        ));
+    return textBox;
   }
+
+  void _submitButtonHandler(String usr, String pwd) {}
 }
-
