9f0a9db88a4b3491f742f513f1cee930f021b1c5
thaorell <khanhchuong7396@gmail.com>
2019-04-13 14:25:45 -0400

    adding themes

---
+++ lib/pages/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = new ThemeData(
+    primarySwatch: Colors.lightGreen,
+    primaryColor: Colors.lightGreen[100],
+    primaryColorBrightness: Brightness.light);
