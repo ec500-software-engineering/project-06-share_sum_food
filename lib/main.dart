c1c171fb1d27c1bc8f18f1389c92cf3d7bd70f37
thaorell <khanhchuong7396@gmail.com>
2019-04-13 16:48:38 -0400

    adding material app stuff

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:share_sum_food/pages/landing_page.dart';
+import 'package:share_sum_food/pages/theme.dart';
 
 bool loggedIn = false;
 
-void main() => runApp(LandingPage());
+void main() => runApp(new MaterialApp(
+      theme: appTheme,
+      home: LandingPage(),
+    ));
