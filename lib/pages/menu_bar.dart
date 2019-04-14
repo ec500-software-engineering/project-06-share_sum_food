b2bcd3f1787942b2fd6e71a104fd1bebeee81460
thaorell <khanhchuong7396@gmail.com>
2019-04-14 11:12:32 -0400

    making the bar looks better

--- lib/pages/menu_bar.dart
+++ lib/pages/menu_bar.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
 
-Widget _menuBar() {
-  final _widgetOptions = [
-    Text('Index 0: Home'),
-    Text('Index 1: List'),
-    Text('Index 2: Settings'),
-  ];
+class bottomBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final _widgetOptions = [
+      Text('Index 0: Home'),
+      Text('Index 1: List'),
+      Text('Index 2: Settings'),
+    ];
 
-  return BottomNavigationBar(
-    items: <BottomNavigationBarItem>[
-      BottomNavigationBarItem(icon: new Icon(Icons.home), title: Text('Home')),
-      BottomNavigationBarItem(icon: new Icon(Icons.list), title: Text('List')),
-      BottomNavigationBarItem(
-          icon: new Icon(Icons.settings), title: Text('Settings')),
-    ],
-    currentIndex: _selectedIndex,
-  );
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+            icon: new Icon(Icons.home), title: Text('Home')),
+        BottomNavigationBarItem(
+            icon: new Icon(Icons.list), title: Text('List')),
+        BottomNavigationBarItem(
+            icon: new Icon(Icons.settings), title: Text('Settings')),
+      ],
+//    currentIndex: _selectedIndex,
+    );
+  }
 }
