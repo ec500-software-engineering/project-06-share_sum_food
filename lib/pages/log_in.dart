e84e0ccaf22e3be779769c8d866fa2ebf635c73c
thaorell <khanhchuong7396@gmail.com>
2019-04-14 11:12:32 -0400

    debug

--- lib/pages/log_in.dart
+++ lib/pages/log_in.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share_sum_food/pages/menu_bar.dart';
 
 class LogIn extends StatefulWidget {
   @override
@@ -18,37 +19,43 @@ class LogInState extends State with TickerProviderStateMixin {
       onPressed: () => _submitButtonHandler(userNameField, passwordField),
     );
     return Scaffold(
+        appBar: new AppBar(
+            title: Text("Sign In >",
+                style: TextStyle(fontSize: 25.0))),
         body: Stack(
-      fit: StackFit.expand,
-      children: <Widget>[
-        Container(
-          margin: new EdgeInsets.only(top: 100.0),
-          child: Column(
-            children: <Widget>[
-              Text("Hello! Sign in to Share Sum Food",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontStyle: FontStyle.italic,
-                      color: Colors.black,
-                      fontSize: 20.0)),
-              Spacer(),
-              Text("Username",
-                  textAlign: TextAlign.left, style: TextStyle(fontSize: 16.0)),
-              Container(
-                  margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 0),
-                  child: _userNameBox()),
-              Text("Password",
-                  textAlign: TextAlign.left, style: TextStyle(fontSize: 16.0)),
-              Container(
-                  margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 10.0),
-                  child: _passwordBox()),
-              Container(child: submitButton),
-              Spacer()
-            ],
-          ),
-        )
-      ],
-    ));
+          fit: StackFit.expand,
+          children: <Widget>[
+            Container(
+              margin: new EdgeInsets.only(top: 100.0),
+              child: Column(
+                children: <Widget>[
+                  Text("Hello! Sign in to Share Sum Food",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          color: Colors.black,
+                          fontSize: 20.0)),
+                  Spacer(),
+                  Text("Username",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16.0)),
+                  Container(
+                      margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 0),
+                      child: _userNameBox()),
+                  Text("Password",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16.0)),
+                  Container(
+                      margin: new EdgeInsets.fromLTRB(50.0, 5.0, 50.0, 10.0),
+                      child: _passwordBox()),
+                  Container(child: submitButton),
+                  Spacer(),
+                  Container(child: bottomBar()),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 
   Widget _userNameBox() {
