b9d7a8287ed7cc93662d29b1b68059d33ed340fb
Huzefa Mandviwala <hzmandviwala@gmail.com>
2019-04-23 14:54:51 -0400

    Fixed map, centered on photonics

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class MapPage extends StatefulWidget {
   @override
@@ -6,12 +8,23 @@ class MapPage extends StatefulWidget {
 }
 
 class MapPageState extends State with TickerProviderStateMixin {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kPhotonics = CameraPosition(
+    target: LatLng(42.349265, -71.1066452),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: new AppBar(
-          title: Text("Map Placeholder",
-              style: TextStyle(fontSize: 25.0))),
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kPhotonics,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
