f81296530d8364030326a0c5352e720858f68827
thaorell <khanhchuong7396@gmail.com>
2019-04-26 17:42:38 -0400

    main linking to different file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:share_sum_food/pages/landing_page.dart';
+import 'package:share_sum_food/pages/log_in.dart';
 import 'package:share_sum_food/pages/theme.dart';
 
 bool loggedIn = false;
 
 void main() => runApp(new MaterialApp(
       theme: appTheme,
-      home: LandingPage(),
+      home: LogIn(),
     ));
