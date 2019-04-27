eefe8f3425b212180414aae1d5d76c4989f227d2
Huzefa Mandviwala <hzmandviwala@gmail.com>
2019-04-26 22:09:11 -0400

    Added functionality to read/write from database. yay crud functions

---
+++ lib/pages/add_food.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'services/crud.dart';
+
+class AddFood extends StatefulWidget{
+  @override
+  _AddFoodState createState() => _AddFoodState();
+}
+
+class _AddFoodState extends State<AddFood> {
+  String foodType;
+  String location;
+
+  crudMethods crudObj = new crudMethods();
+
+  Future<bool> addDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Add Data', style: TextStyle(fontSize: 15.0)),
+            content: Column(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(hintText: 'Enter Food Type'),
+                  onChanged: (value) {
+                    this.foodType = value;
+                  },
+                ),
+                SizedBox(height: 5.0),
+                TextField(
+                  decoration: InputDecoration(hintText: 'Enter Location'),
+                  onChanged: (value) {
+                    this.location = value;
+                  },
+                ),
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Add'),
+                textColor: Colors.blue,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  crudObj.addData({
+                    'FoodType': this.foodType,
+                    'Location': this.location
+                  }).then((result) {
+                    dialogTrigger(context);
+                  }).catchError((e) {
+                    print(e);
+                  });
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  Future<bool> dialogTrigger(BuildContext context) async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Job Done', style: TextStyle(fontSize: 15.0)),
+            content: Text('Added'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Alright'),
+                textColor: Colors.blue,
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build (BuildContext context){
+     return new Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                addDialog(context);
+              },
+            )
+          ],
+        ),
+        body: Center());
+  }
+}
