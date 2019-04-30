67aa7e7014fea4f6252da1bc2e114f9df4839a80
Huzefa Mandviwala <hzmandviwala@gmail.com>
2019-04-30 12:24:56 -0400

    Added map, fixed build, map centers on current location

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 import 'dart:async';
 
 class MapPage extends StatefulWidget {
@@ -8,23 +9,61 @@ class MapPage extends StatefulWidget {
 }
 
 class MapPageState extends State with TickerProviderStateMixin {
-  Completer<GoogleMapController> _controller = Completer();
+  
+  GoogleMapController mapController;
+  var currentLocation;
+  bool mapToggle = false;
+
+  
+  void initState() {
+    super.initState();
+    Geolocator().getCurrentPosition().then((currloc) {
+      setState(() {
+        currentLocation = currloc;
+        mapToggle = true;
+      });
+    });
+  }
 
   static final CameraPosition _kPhotonics = CameraPosition(
-    target: LatLng(42.349265, -71.1066452),
+    target: LatLng(42, -71.1066452),
     zoom: 14.4746,
   );
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _kPhotonics,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+    body: Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height - 60.0,
+              width: double.infinity,
+              child: mapToggle ?
+              GoogleMap(
+                onMapCreated: onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(currentLocation.latitude, currentLocation.longitude),
+                  zoom: 14.0,
+                ),
+              ):
+              Center(child:
+              Text('Loading map...',
+              style: TextStyle(
+                fontSize: 20.0
+              ))),
+            )
+          ],
+        )
+      ],
+    )
     );
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+    });
   }
 }
