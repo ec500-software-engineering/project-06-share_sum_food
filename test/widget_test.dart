0215a62f16f1f51615b8c3e280b7385a4134d457
Huzefa Mandviwala <hzmandviwala@gmail.com>
2019-05-02 09:53:20 -0400

    Fixed annoying errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:share_sum_food/pages/home.dart';
 
 import 'package:share_sum_food/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Home());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
