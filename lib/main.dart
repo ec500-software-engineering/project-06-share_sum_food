120690573df62383a7d2433a33e8237479dd8494
thaorell <khanhchuong7396@gmail.com>
2019-05-04 13:12:39 -0400

    using preferences to direct main page

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,39 @@ import 'package:flutter/material.dart';
 import 'package:share_sum_food/pages/log_in.dart';
 import 'package:share_sum_food/pages/theme.dart';
 import 'package:share_sum_food/pages/add_food.dart';
-import 'package:share_sum_food/globals.dart';
 import 'package:share_sum_food/pages/list.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:share_sum_food/pages/map_page.dart';
+import 'package:share_sum_food/pages/home.dart';
 
 bool loggedIn = false;
 
-void main(){
-  if (!Globals().isLoggedIn){
+void main() async{
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var email = prefs.getString("email");
+  print(email);
+  var isFoodSeeker = prefs.getBool("isFoodSeeker");
+  print(isFoodSeeker);
+  //this means the user is logged in
+  if (email != null && isFoodSeeker != null){
+    if (isFoodSeeker){
+      runApp(new MaterialApp(theme: appTheme,
+        home: Home()
+      ));
+    }
+    else if (!isFoodSeeker){
+      runApp(new MaterialApp(theme: appTheme,
+          home: AddFood()
+      ));
+    }
+  }
+
+  else if (email == null || isFoodSeeker == null) {
     runApp(new MaterialApp(theme: appTheme,
-      home: LogIn(),
+      home: LogIn()
     ));
   }
-  else if (Globals().isLoggedIn && (Globals().isDonor && !Globals().isSeeker)){
-    runApp(new MaterialApp(theme: appTheme,
-      home: AddFood(),
-    ));
-  }
-  else if (Globals().isLoggedIn && (!Globals().isDonor && Globals().isSeeker)){
-    runApp(new MaterialApp(theme: appTheme,
-      home: List(),
-    ));
-  }
+
 
 }
 
