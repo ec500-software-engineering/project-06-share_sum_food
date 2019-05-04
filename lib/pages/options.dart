ae579bf89cce231c0ff81198422225aae15f0c5d
thaorell <khanhchuong7396@gmail.com>
2019-05-04 12:47:52 -0400

    adding option page

--- lib/pages/options.dart
+++ lib/pages/options.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:share_sum_food/pages/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:share_sum_food/pages/log_in.dart';
 
 class Options extends StatefulWidget {
   @override
@@ -10,6 +11,34 @@ class Options extends StatefulWidget {
 class OptionsState extends State with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Options>"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            MaterialButton(
+              height: 50.0,
+              minWidth: 80.0,
+              color: Theme.of(context).buttonColor,
+              textColor: Colors.black,
 
+              child: new Text("Sign Out"),
+              onPressed: _onHandlingSignOut,
+              splashColor: Colors.redAccent,
+            ),
+          ],
+        ),
+      )
+    );
+  }
+
+  _onHandlingSignOut() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.clear();
+    runApp(MaterialApp(home: LogIn(), theme: Theme.of(context)));
   }
 }
