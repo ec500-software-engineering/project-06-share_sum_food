63cfec0773f3b10df365046f08cc4e0584c551cd
thaorell <khanhchuong7396@gmail.com>
2019-05-05 19:46:16 -0400

    added global var with status

--- lib/pages/add_food.dart
+++ lib/pages/add_food.dart
@@ -3,13 +3,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'services/crud.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AddFood extends StatefulWidget{
+  final bool isSeeker;
+  const AddFood({@required this.isSeeker});
+
   @override
-  _AddFoodState createState() => _AddFoodState();
+  _AddFoodState createState() => _AddFoodState(isSeeker);
 }
 
 class _AddFoodState extends State<AddFood> {
+  bool isSeeker;
+   _AddFoodState(bool _isSeeker){
+    this.isSeeker = _isSeeker;
+   }
+
   String foodType;
   String location;
 
@@ -50,6 +59,14 @@ class _AddFoodState extends State<AddFood> {
       );
     }
   }
+
+//
+//  Future<bool> _checkIfSeeker() async{
+//    SharedPreferences prefs = await SharedPreferences.getInstance();
+//    var value=  prefs.getBool("isFoodSeeker");
+//    print("MY STATUS IS " + (value).toString());
+//    return value;
+//  }
 
   Future<bool> addDialog(BuildContext context) async {
     return showDialog(
@@ -129,18 +146,37 @@ class _AddFoodState extends State<AddFood> {
     });
     super.initState();
   }
+
+//  var isSeeker;
+
+//  Widget _addIcon(){
+//    var _icon;
+//    this._checkIfSeeker().then((results) {
+//      print("MY RESULTS ARE " + results.toString());
+//      isSeeker = results;
+//      return _icon = (!isSeeker) ? IconButton(
+//          icon: Icon(Icons.add),
+//          onPressed: () {
+//            addDialog(context);
+//          }
+//      ): Text("");
+//  });
+//  }
+
+
   @override
-  Widget build (BuildContext context){
-     return new Scaffold(
+  Widget build (BuildContext context) {
+      print("ADD FOOD IS " + isSeeker.toString());
+      return new Scaffold(
         appBar: AppBar(
           title: Text('Share Sum Food'),
           actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                addDialog(context);
-              },
-            ),
+            (this.isSeeker) ? Text("") : IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            addDialog(context);
+            }
+          ) ,
             IconButton(
               icon: Icon(Icons.refresh),
               onPressed: () {
@@ -156,11 +192,11 @@ class _AddFoodState extends State<AddFood> {
         body: Column(
           children: <Widget>[
             Flexible(
-              child: _foodList()
+                child: _foodList()
             ),
           ],
         ),
-     );
+      );
   }
 
   Widget _foodList(){
